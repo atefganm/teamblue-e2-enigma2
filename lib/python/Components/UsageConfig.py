ccf03864487bc7bd305af38abaa00582330aa813
atefganm <atef_anim_1974@yahoo.com>
2024-04-29 12:08:52 +0300

    Add Option Zap channels Mode

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -654,6 +654,12 @@ def InitUsageConfig():
 			("mute", _("Black screen")), ("hold", _("Hold screen")), ("mutetilllock", _("Black screen till locked")), ("holdtilllock", _("Hold till locked"))])
 		config.misc.zapmode.addNotifier(setZapmode, immediate_feedback=False)
 
+	if not BoxInfo.getItem("ZapMode") and exists("/proc/stb/info/model"):
+		def setZapmodeDM(el):
+			print('[UsageConfig] >>> zapmodeDM')
+		config.misc.zapmodeDM = ConfigSelection(default="black", choices=[("black", _("Black screen")), ("hold", _("Hold screen"))])
+		config.misc.zapmodeDM.addNotifier(setZapmodeDM, immediate_feedback = False)
+
 	if BoxInfo.getItem("VFD_scroll_repeats"):
 		def scroll_repeats(el):
 			open(BoxInfo.getItem("VFD_scroll_repeats"), "w").write(el.value)
