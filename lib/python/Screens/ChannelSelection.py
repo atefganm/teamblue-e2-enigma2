44cd8819db7d7ba69a0069d7a09d21bf040f5a02
atefganm <atef_anim_1974@yahoo.com>
2025-01-10 10:26:31 +0300

    SimpleChannelSelection - added epg button for select channel too (#4146) Co-authored-by: ims <ims21@users.sourceforge.net> (cherry picked from commit fdfa6358abac9ab516116a8165943fec6d8e6696) Thanks to @ims21

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -2793,6 +2793,7 @@ class SimpleChannelSelection(ChannelSelectionBase, SelectionEventInfo):
 			{
 				"cancel": self.close,
 				"ok": self.channelSelected,
+				"epg": self.channelSelected,
 				"keyRadio": self.setModeRadio,
 				"keyTV": self.setModeTv,
 				"toggleTvRadio": self.toggleTVRadio,
