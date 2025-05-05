ba9ec3fac84bf0b0dbb5a9c593aaa1a9a5025e0d
atefganm <atef_anim_1974@yahoo.com>
2025-05-05 22:17:03 +0300

    Revert 'Resolve possible GSOD when opening MainMenu.' Issue was caused by a skin change, and needs to be fixed there.
    This reverts commit d7275db.
    Thanks to @WanWizard

--- lib/python/Components/Addons/MainMenu.py
+++ lib/python/Components/Addons/MainMenu.py
@@ -78,9 +78,6 @@ class MainMenu(GUIAddon):
 	def selectionChanged(self):
 		if self.instance and hasattr(self, "source"):
 			self.source.setConnectedGuiElement(self)
-	def setFont(self, value):
-		self.font = parseFont(value, ((1, 1), (1, 1)))
-		self.l.setFont(0, self.font)
 
 	def setMinWidth(self, value):
 		self.minWidth = parseScale(value)
